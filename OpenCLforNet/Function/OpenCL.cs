34779a3b3126744ff74ba4bc3ff329ffa46348a2
204504bySE <204504byse@204504byse.info>
2021-05-28 02:09:00 +0900

    OpenCL.dll→OpenCL (linux compatibility: try later)

--- OpenCLforNet/Function/OpenCL.cs
+++ OpenCLforNet/Function/OpenCL.cs
@@ -13,106 +13,106 @@ namespace OpenCLforNet.Function
     unsafe static class OpenCL
     {
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetPlatformIDs(uint num_entries, void** platforms, uint* num_platforms);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetPlatformInfo(void* platform, cl_platform_info param_name, size_t param_value_size, void* param_value, void* param_value_size_ret);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetDeviceIDs(void* platform, cl_device_type device_type, uint num_entries, void** devices, uint* num_devices);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetDeviceInfo(void* device, cl_device_info param_name, size_t param_value_size, void* param_value, void* param_value_size_ret);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clCreateContext(void* properties, uint num_devices, void** devices, void* notify, void* user_data, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseContext(void* context);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clCreateCommandQueue(void* context, void* device, cl_command_queue_properties properties, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseCommandQueue(void* command_queue);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clCreateProgramWithSource(void* context, int count, byte** strings, void* lengths, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clBuildProgram(void* program, uint num_devices, void** device_list, byte* options, void* notify, void* user_data);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetProgramBuildInfo(void* program, void* device, cl_program_build_info param_name, size_t param_value_size, void* param_value, void* param_value_size_ret);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseProgram(void* program);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clCreateKernel(void* program, byte* kernel_name, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseKernel(void* kernel);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clCreateBuffer(void* context, cl_mem_flags flags, size_t size, void* host_ptr, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueReadBuffer(void* command_queue, void* buffer, bool blocking_read, size_t offset, size_t cb, void* ptr, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueWriteBuffer(void* command_queue, void* buffer, bool blocking_write, size_t offset, size_t cb, void* ptr, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clEnqueueMapBuffer(void* command_queue, void* buffer, bool blocking_map, cl_map_flags map_flags, size_t offset, size_t size, uint num_events_in_wait_list, void* event_wait_list, void** event_, cl_status_code* error_code);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueUnmapMemObject(void* command_queue, void* memobj, void* mapped_ptr, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueCopyBuffer(void* command_queue, void* src_buffer, void* dst_buffer, size_t src_offset, size_t dst_offset, size_t cb, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseMemObject(void* mem);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void* clSVMAlloc(void* context, cl_mem_flags flags, size_t size, uint alignment);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueSVMMemcpy(void* command_queue, bool blocking_copy, void* dst_ptr, void* src_ptr, size_t size, uint num_events_in_wait_list, void* event_wait_list, void** _event);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueSVMMap(void* command_queue, bool blocking_map, cl_map_flags map_flags, void* svm_ptr, size_t size, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueSVMUnmap(void* command_queue, void* svm_ptr, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern void clSVMFree(void* context, void* svm_pointer);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clSetKernelArg(void* kernel, int arg_index, int arg_size, void* arg_value);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clSetKernelArgSVMPointer(void* kernel, int arg_index, void* arg_value);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clEnqueueNDRangeKernel(void* command_queue, void* kernel, uint work_dim, size_t* global_work_offset, size_t* gloal_work_size, size_t* local_work_size, uint num_events_in_wait_list, void* event_wait_list, void** event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clWaitForEvents(uint num_events, void** event_list);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clGetEventProfilingInfo(void* event_, cl_profiling_info param_name, size_t param_value_size, void* param_value, size_t* param_value_size_ret);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clReleaseEvent(void* event_);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clFlush(void* command_queue);
 
-        [DllImport("OpenCL.dll")]
+        [DllImport("OpenCL")]
         public static extern cl_status_code clFinish(void* command_queue);
 
     }
